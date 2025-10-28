7d89a0bd6cab3579f70f7e557d332ab7638f6f92
4r33x <vonauwers@gmail.com>
2025-10-28 17:57:47 +0300

    Refactor proto message derive into modules

---
+++ crates/prosto_derive/src/proto_message/structs.rs
@@ -0,0 +1,206 @@
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::DeriveInput;
+use syn::ItemStruct;
+
+use super::unified_field_handler::FieldAccess;
+use super::unified_field_handler::FieldInfo;
+use super::unified_field_handler::assign_tags;
+use super::unified_field_handler::build_clear_stmts;
+use super::unified_field_handler::build_encode_stmts;
+use super::unified_field_handler::build_encoded_len_terms;
+use super::unified_field_handler::build_is_default_checks;
+use super::unified_field_handler::build_proto_default_expr;
+use super::unified_field_handler::generate_proto_shadow_impl;
+use super::unified_field_handler::strip_proto_attrs;
+use crate::parse::UnifiedProtoConfig;
+use crate::utils::parse_field_config;
+
+pub(super) fn generate_struct_impl(input: &DeriveInput, item_struct: &ItemStruct, data: &syn::DataStruct, config: &UnifiedProtoConfig) -> TokenStream2 {
+    let name = &input.ident;
+    let generics = &input.generics;
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let struct_item = sanitize_struct(item_struct.clone());
+
+    let fields = match &data.fields {
+        syn::Fields::Named(named) => named
+            .named
+            .iter()
+            .enumerate()
+            .map(|(idx, field)| FieldInfo {
+                index: idx,
+                field,
+                access: FieldAccess::Named(field.ident.as_ref().expect("named field missing ident")),
+                config: parse_field_config(field),
+                tag: None,
+            })
+            .collect::<Vec<_>>(),
+        syn::Fields::Unnamed(unnamed) => unnamed
+            .unnamed
+            .iter()
+            .enumerate()
+            .map(|(idx, field)| FieldInfo {
+                index: idx,
+                field,
+                access: FieldAccess::Tuple(idx),
+                config: parse_field_config(field),
+                tag: None,
+            })
+            .collect::<Vec<_>>(),
+        syn::Fields::Unit => Vec::new(),
+    };
+
+    let fields = assign_tags(fields);
+
+    let proto_shadow_impl = if config.sun.is_some() {
+        quote! {}
+    } else {
+        generate_proto_shadow_impl(name, generics)
+    };
+
+    let proto_ext_impl = generate_proto_ext_impl(name, &impl_generics, &ty_generics, where_clause, &fields, config);
+    let proto_wire_impl = generate_proto_wire_impl(name, &impl_generics, &ty_generics, where_clause, &fields);
+
+    quote! {
+        #struct_item
+        #proto_shadow_impl
+        #proto_ext_impl
+        #proto_wire_impl
+    }
+}
+
+fn sanitize_struct(mut item: ItemStruct) -> ItemStruct {
+    item.attrs = strip_proto_attrs(&item.attrs);
+    match &mut item.fields {
+        syn::Fields::Named(named) => {
+            for field in &mut named.named {
+                field.attrs = strip_proto_attrs(&field.attrs);
+            }
+        }
+        syn::Fields::Unnamed(unnamed) => {
+            for field in &mut unnamed.unnamed {
+                field.attrs = strip_proto_attrs(&field.attrs);
+            }
+        }
+        syn::Fields::Unit => {}
+    }
+    item
+}
+
+fn generate_proto_ext_impl(
+    name: &syn::Ident,
+    impl_generics: &syn::ImplGenerics,
+    ty_generics: &syn::TypeGenerics,
+    where_clause: Option<&syn::WhereClause>,
+    fields: &[FieldInfo<'_>],
+    config: &UnifiedProtoConfig,
+) -> TokenStream2 {
+    let target_ty = if let Some(sun) = &config.sun {
+        let ty = &sun.ty;
+        quote! { #ty }
+    } else {
+        quote! { #name #ty_generics }
+    };
+
+    let decode_arms = fields
+        .iter()
+        .filter_map(|info| {
+            let tag = info.tag?;
+            let field_ty = &info.field.ty;
+            let access = info.access.access_tokens(quote! { value });
+            Some(quote! {
+                #tag => {
+                    <#field_ty as ::proto_rs::ProtoWire>::decode_into(
+                        wire_type,
+                        &mut #access,
+                        buf,
+                        ctx,
+                    )
+                }
+            })
+        })
+        .collect::<Vec<_>>();
+
+    let shadow_ty = quote! { #name #ty_generics };
+
+    quote! {
+        impl #impl_generics ::proto_rs::ProtoExt for #target_ty #where_clause {
+            type Shadow<'b> = #shadow_ty where Self: 'b;
+
+            #[inline(always)]
+            fn merge_field(
+                value: &mut Self::Shadow<'_>,
+                tag: u32,
+                wire_type: ::proto_rs::encoding::WireType,
+                buf: &mut impl ::proto_rs::bytes::Buf,
+                ctx: ::proto_rs::encoding::DecodeContext,
+            ) -> Result<(), ::proto_rs::DecodeError> {
+                match tag {
+                    #(#decode_arms,)*
+                    _ => ::proto_rs::encoding::skip_field(wire_type, tag, buf, ctx),
+                }
+            }
+        }
+    }
+}
+
+fn generate_proto_wire_impl(name: &syn::Ident, impl_generics: &syn::ImplGenerics, ty_generics: &syn::TypeGenerics, where_clause: Option<&syn::WhereClause>, fields: &[FieldInfo<'_>]) -> TokenStream2 {
+    let proto_default_expr = build_proto_default_expr(fields);
+    let self_tokens = quote! { self };
+    let clear_stmts = build_clear_stmts(fields, &self_tokens);
+    let encode_input_tokens = quote! { value };
+    let is_default_checks = build_is_default_checks(fields, &encode_input_tokens);
+    let encoded_len_terms = build_encoded_len_terms(fields, &encode_input_tokens);
+    let encode_stmts = build_encode_stmts(fields, &encode_input_tokens);
+
+    quote! {
+        impl #impl_generics ::proto_rs::ProtoWire for #name #ty_generics #where_clause {
+            type EncodeInput<'b> = &'b Self;
+            const KIND: ::proto_rs::ProtoKind = ::proto_rs::ProtoKind::Message;
+
+            #[inline(always)]
+            fn proto_default() -> Self {
+                #proto_default_expr
+            }
+
+            #[inline(always)]
+            fn clear(&mut self) {
+                #(#clear_stmts;)*
+            }
+
+            #[inline(always)]
+            fn is_default_impl(value: &Self::EncodeInput<'_>) -> bool {
+                #(#is_default_checks;)*
+                true
+            }
+
+            #[inline(always)]
+            unsafe fn encoded_len_impl_raw(value: &Self::EncodeInput<'_>) -> usize {
+                0 #(+ #encoded_len_terms)*
+            }
+
+            #[inline(always)]
+            fn encode_raw_unchecked(
+                value: Self::EncodeInput<'_>,
+                buf: &mut impl ::proto_rs::bytes::BufMut,
+            ) {
+                #(#encode_stmts)*
+            }
+
+            #[inline(always)]
+            fn decode_into(
+                wire_type: ::proto_rs::encoding::WireType,
+                value: &mut Self,
+                buf: &mut impl ::proto_rs::bytes::Buf,
+                ctx: ::proto_rs::encoding::DecodeContext,
+            ) -> Result<(), ::proto_rs::DecodeError> {
+                ::proto_rs::encoding::check_wire_type(
+                    ::proto_rs::encoding::WireType::LengthDelimited,
+                    wire_type,
+                )?;
+                <Self as ::proto_rs::ProtoExt>::merge_length_delimited(value, buf, ctx)
+            }
+        }
+    }
+}
---
+++ crates/prosto_derive/src/proto_message/unified_field_handler.rs
@@ -0,0 +1,256 @@
+use std::collections::BTreeSet;
+
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::Attribute;
+use syn::Field;
+use syn::Ident;
+use syn::ItemEnum;
+use syn::Type;
+use syn::spanned::Spanned;
+
+use crate::utils::FieldConfig;
+use crate::utils::is_option_type;
+
+#[derive(Clone)]
+pub struct FieldInfo<'a> {
+    pub index: usize,
+    pub field: &'a Field,
+    pub access: FieldAccess<'a>,
+    pub config: FieldConfig,
+    pub tag: Option<u32>,
+}
+
+#[derive(Clone)]
+pub enum FieldAccess<'a> {
+    Named(&'a Ident),
+    Tuple(usize),
+    Direct(TokenStream2),
+}
+
+impl FieldAccess<'_> {
+    pub fn ident(&self) -> Option<&Ident> {
+        match self {
+            FieldAccess::Named(id) => Some(id),
+            FieldAccess::Tuple(_) | FieldAccess::Direct(_) => None,
+        }
+    }
+
+    pub fn access_tokens(&self, base: TokenStream2) -> TokenStream2 {
+        match self {
+            FieldAccess::Named(ident) => quote! { #base.#ident },
+            FieldAccess::Tuple(idx) => {
+                let index = syn::Index::from(*idx);
+                quote! { #base.#index }
+            }
+            FieldAccess::Direct(tokens) => tokens.clone(),
+        }
+    }
+}
+
+pub fn strip_proto_attrs(attrs: &[Attribute]) -> Vec<Attribute> {
+    attrs.iter().filter(|attr| !attr.path().is_ident("proto_message") && !attr.path().is_ident("proto")).cloned().collect()
+}
+
+pub fn sanitize_enum(mut item: ItemEnum) -> ItemEnum {
+    item.attrs = strip_proto_attrs(&item.attrs);
+    for variant in &mut item.variants {
+        variant.attrs = strip_proto_attrs(&variant.attrs);
+    }
+    item
+}
+
+pub fn assign_tags(mut fields: Vec<FieldInfo<'_>>) -> Vec<FieldInfo<'_>> {
+    let mut used = BTreeSet::new();
+    let mut next = 1u32;
+
+    for info in &mut fields {
+        if info.config.skip {
+            continue;
+        }
+
+        if info.config.into_type.is_some()
+            || info.config.from_type.is_some()
+            || info.config.into_fn.is_some()
+            || info.config.from_fn.is_some()
+            || info.config.skip_deser_fn.is_some()
+            || info.config.is_rust_enum
+            || info.config.is_message
+            || info.config.is_proto_enum
+        {
+            panic!("proto_message rewrite does not yet support advanced field attributes");
+        }
+
+        let tag = if let Some(custom) = info.config.custom_tag {
+            assert!(custom != 0, "proto field tags must be >= 1");
+            let custom_u32: u32 = custom.try_into().expect("proto field tag overflowed u32");
+            assert!(used.insert(custom_u32), "duplicate proto field tag: {custom}");
+            custom_u32
+        } else {
+            while used.contains(&next) {
+                next = next.checked_add(1).expect("proto field tag overflowed u32");
+            }
+            let assigned = next;
+            used.insert(assigned);
+            next = next.checked_add(1).expect("proto field tag overflowed u32");
+            assigned
+        };
+
+        info.tag = Some(tag);
+    }
+
+    fields
+}
+
+pub fn generate_proto_shadow_impl(name: &Ident, generics: &syn::Generics) -> TokenStream2 {
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+    quote! {
+        impl #impl_generics ::proto_rs::ProtoShadow for #name #ty_generics #where_clause {
+            type Sun<'a> = &'a Self;
+            type OwnedSun = Self;
+            type View<'a> = &'a Self;
+
+            #[inline(always)]
+            fn to_sun(self) -> Result<Self::OwnedSun, ::proto_rs::DecodeError> {
+                Ok(self)
+            }
+
+            #[inline(always)]
+            fn from_sun(value: Self::Sun<'_>) -> Self::View<'_> {
+                value
+            }
+        }
+    }
+}
+
+pub struct EncodeBinding {
+    pub init: TokenStream2,
+    pub ident: Ident,
+}
+
+pub fn encode_input_binding(field: &FieldInfo<'_>, base: &TokenStream2) -> EncodeBinding {
+    let ty = &field.field.ty;
+    let binding_ident = Ident::new(&format!("__proto_rs_field_{}_input", field.index), field.field.span());
+    let access_expr = match &field.access {
+        FieldAccess::Direct(tokens) => tokens.clone(),
+        _ => field.access.access_tokens(base.clone()),
+    };
+
+    let init_expr = if is_option_type(ty) {
+        quote! { (#access_expr).as_ref().map(|inner| inner) }
+    } else if matches!(field.access, FieldAccess::Direct(_)) {
+        access_expr.clone()
+    } else if is_value_encode_type(ty) {
+        access_expr.clone()
+    } else {
+        quote! { &(#access_expr) }
+    };
+
+    let init = quote! {
+        let #binding_ident: <#ty as ::proto_rs::ProtoWire>::EncodeInput<'_> = #init_expr;
+    };
+
+    EncodeBinding { init, ident: binding_ident }
+}
+
+fn is_value_encode_type(ty: &Type) -> bool {
+    matches!(ty, Type::Path(type_path)
+    if type_path.qself.is_none()
+        && type_path.path.segments.len() == 1
+        && matches!(type_path.path.segments[0].ident.to_string().as_str(),
+            "bool" | "i8" | "i16" | "i32" | "i64" | "i128" | "isize" |
+            "u8" | "u16" | "u32" | "u64" | "u128" | "usize" |
+            "f32" | "f64"
+        ))
+}
+
+pub fn build_proto_default_expr(fields: &[FieldInfo<'_>]) -> TokenStream2 {
+    if fields.is_empty() {
+        return quote! { Self };
+    }
+
+    if fields.iter().all(|f| matches!(f.access, FieldAccess::Tuple(_))) {
+        let defaults = fields.iter().map(|info| {
+            let ty = &info.field.ty;
+            quote! { <#ty as ::proto_rs::ProtoWire>::proto_default() }
+        });
+        quote! { Self( #(#defaults),* ) }
+    } else {
+        let defaults = fields.iter().map(|info| {
+            let ident = info.access.ident().expect("expected named field");
+            let ty = &info.field.ty;
+            quote! { #ident: <#ty as ::proto_rs::ProtoWire>::proto_default() }
+        });
+        quote! { Self { #(#defaults),* } }
+    }
+}
+
+pub fn build_clear_stmts(fields: &[FieldInfo<'_>], self_tokens: &TokenStream2) -> Vec<TokenStream2> {
+    fields
+        .iter()
+        .map(|info| {
+            let access = info.access.access_tokens(self_tokens.clone());
+            let ty = &info.field.ty;
+            quote! { <#ty as ::proto_rs::ProtoWire>::clear(&mut #access) }
+        })
+        .collect()
+}
+
+pub fn build_is_default_checks(fields: &[FieldInfo<'_>], base: &TokenStream2) -> Vec<TokenStream2> {
+    fields
+        .iter()
+        .filter_map(|info| {
+            info.tag?;
+            let ty = &info.field.ty;
+            let binding = encode_input_binding(info, base);
+            let ident = binding.ident;
+            let init = binding.init;
+            Some(quote! {
+                {
+                    #init
+                    if !<#ty as ::proto_rs::ProtoWire>::is_default_impl(&#ident) {
+                        return false;
+                    }
+                }
+            })
+        })
+        .collect()
+}
+
+pub fn build_encoded_len_terms(fields: &[FieldInfo<'_>], base: &TokenStream2) -> Vec<TokenStream2> {
+    fields
+        .iter()
+        .filter_map(|info| {
+            let tag = info.tag?;
+            let ty = &info.field.ty;
+            let binding = encode_input_binding(info, base);
+            let ident = binding.ident;
+            let init = binding.init;
+            Some(quote! {{
+                #init
+                <#ty as ::proto_rs::ProtoWire>::encoded_len_tagged_impl(&#ident, #tag)
+            }})
+        })
+        .collect()
+}
+
+pub fn build_encode_stmts(fields: &[FieldInfo<'_>], base: &TokenStream2) -> Vec<TokenStream2> {
+    fields
+        .iter()
+        .filter_map(|info| {
+            let tag = info.tag?;
+            let ty = &info.field.ty;
+            let binding = encode_input_binding(info, base);
+            let ident = binding.ident;
+            let init = binding.init;
+            Some(quote! {
+                {
+                    #init
+                    if let Err(err) = <#ty as ::proto_rs::ProtoWire>::encode_with_tag(#tag, #ident, buf) {
+                        panic!("encode_raw_unchecked called without sufficient capacity: {err}");
+                    }
+                }
+            })
+        })
+        .collect()
+}
