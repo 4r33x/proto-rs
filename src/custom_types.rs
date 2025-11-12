ef8a2ca85551db9339ff73183a30a71d5c011c25
4r33x <vonauwers@gmail.com>
2025-11-12 04:03:08 +0300

    add chrono impl

--- src/custom_types.rs
+++ src/custom_types.rs
@@ -3,3 +3,6 @@ pub mod fastnum;
 
 #[cfg(feature = "solana")]
 pub mod solana;
+
+#[cfg(feature = "chrono")]
+pub mod chrono;
---
+++ src/custom_types/chrono.rs
@@ -0,0 +1,31 @@
+use chrono::DateTime;
+use chrono::Utc;
+
+use crate::DecodeError;
+use crate::ProtoShadow;
+use crate::proto_message;
+
+#[proto_message(proto_path = "protos/chrono.proto", sun = DateTime<Utc>)]
+struct DateTimeProto {
+    #[proto(tag = 1)]
+    pub secs: i64,
+    #[proto(tag = 2)]
+    pub ns: u32,
+}
+
+impl ProtoShadow<DateTime<Utc>> for DateTimeProto {
+    type Sun<'a> = &'a DateTime<Utc>;
+    type OwnedSun = DateTime<Utc>;
+    type View<'a> = Self;
+
+    fn to_sun(self) -> Result<Self::OwnedSun, DecodeError> {
+        DateTime::from_timestamp(self.secs, self.ns).ok_or(DecodeError::new("failed to decode  DateTime"))
+    }
+
+    fn from_sun(value: Self::Sun<'_>) -> Self::View<'_> {
+        Self {
+            secs: value.timestamp(),
+            ns: value.timestamp_subsec_nanos(),
+        }
+    }
+}
