ffce37a9ed72b9a5488a345b36fa544b39a1a357
4r33x <vonauwers@gmail.com>
2026-01-13 12:57:49 +0300

    optimize Mutex

--- src/wrappers/mutexes.rs
+++ src/wrappers/mutexes.rs
@@ -19,15 +19,6 @@ impl<G> MutexGuardEncodeInput<G> {
     }
 }
 
-#[inline(always)]
-fn encode_input_from_guard<'a, T, G>(guard: &'a G) -> T::EncodeInput<'a>
-where
-    T: ProtoWire + EncodeInputFromRef<'a> + 'a,
-    G: std::ops::Deref<Target = T>,
-{
-    T::encode_input_from_ref(&*guard)
-}
-
 type StdMutexEncodeInput<'a, T> = MutexGuardEncodeInput<std::sync::MutexGuard<'a, T>>;
 
 impl<'a, T> EncodeInputFromRefValue<'a, std::sync::Mutex<T>> for StdMutexEncodeInput<'a, T> {
@@ -68,13 +59,13 @@ where
 
     #[inline(always)]
     unsafe fn encoded_len_impl_raw(value: &Self::EncodeInput<'_>) -> usize {
-        let input = encode_input_from_guard::<T, _>(&value.0);
+        let input = T::encode_input_from_ref(&value.0);
         unsafe { T::encoded_len_impl_raw(&input) }
     }
 
     #[inline(always)]
     fn encode_raw_unchecked(value: Self::EncodeInput<'_>, buf: &mut impl BufMut) {
-        let input = encode_input_from_guard::<T, _>(&value.0);
+        let input = T::encode_input_from_ref(&value.0);
         T::encode_raw_unchecked(input, buf);
     }
 
@@ -86,7 +77,7 @@ where
 
     #[inline(always)]
     fn is_default_impl(value: &Self::EncodeInput<'_>) -> bool {
-        let input = encode_input_from_guard::<T, _>(&value.0);
+        let input = T::encode_input_from_ref(&value.0);
         T::is_default_impl(&input)
     }
 
@@ -102,94 +93,6 @@ where
         }
     }
 }
-
-pub struct StdMutexShadow<S>(pub S);
-
-// impl<T> ProtoExt for std::sync::Mutex<T>
-// where
-//     T: ProtoExt,
-//     for<'a> T: 'a,
-// {
-//     type Shadow<'a>
-//         = StdMutexShadow<<T as ProtoExt>::Shadow<'a>>
-//     where
-//         T: 'a;
-
-//     #[inline(always)]
-//     fn merge_field(
-//         value: &mut Self::Shadow<'_>,
-//         tag: u32,
-//         wire: WireType,
-//         buf: &mut impl Buf,
-//         ctx: DecodeContext,
-//     ) -> Result<(), DecodeError> {
-//         if tag == 1 {
-//             T::merge_field(&mut value.0, tag, wire, buf, ctx)
-//         } else {
-//             crate::encoding::skip_field(wire, tag, buf, ctx)
-//         }
-//     }
-// }
-
-impl<SHD> ProtoWire for StdMutexShadow<SHD>
-where
-    SHD: ProtoWire,
-{
-    type EncodeInput<'b> = <SHD as ProtoWire>::EncodeInput<'b>;
-    const KIND: ProtoKind = SHD::KIND;
-
-    #[inline(always)]
-    unsafe fn encoded_len_impl_raw(value: &Self::EncodeInput<'_>) -> usize {
-        unsafe { SHD::encoded_len_impl_raw(value) }
-    }
-
-    #[inline(always)]
-    fn encode_raw_unchecked(value: Self::EncodeInput<'_>, buf: &mut impl BufMut) {
-        SHD::encode_raw_unchecked(value, buf);
-    }
-
-    #[inline(always)]
-    fn decode_into(wire_type: WireType, value: &mut Self, buf: &mut impl Buf, ctx: DecodeContext) -> Result<(), DecodeError> {
-        SHD::decode_into(wire_type, &mut value.0, buf, ctx)
-    }
-
-    #[inline(always)]
-    fn is_default_impl(value: &Self::EncodeInput<'_>) -> bool {
-        SHD::is_default_impl(value)
-    }
-
-    #[inline(always)]
-    fn proto_default() -> Self {
-        StdMutexShadow(SHD::proto_default())
-    }
-
-    #[inline(always)]
-    fn clear(&mut self) {
-        SHD::clear(&mut self.0);
-    }
-}
-
-impl<SHD, T> ProtoShadow<std::sync::Mutex<T>> for StdMutexShadow<SHD>
-where
-    SHD: ProtoShadow<T, OwnedSun = T>,
-{
-    type Sun<'a> = SHD::Sun<'a>;
-    type View<'a> = SHD::View<'a>;
-    type OwnedSun = std::sync::Mutex<T>;
-
-    #[inline(always)]
-    fn to_sun(self) -> Result<Self::OwnedSun, DecodeError> {
-        Ok(std::sync::Mutex::new(self.0.to_sun()?))
-    }
-
-    #[inline(always)]
-    fn from_sun(value: Self::Sun<'_>) -> Self::View<'_> {
-        SHD::from_sun(value)
-    }
-}
-
-#[cfg(feature = "parking_lot")]
-pub struct ParkingLotMutexShadow<S>(pub S);
 
 #[cfg(feature = "parking_lot")]
 type ParkingLotMutexEncodeInput<'a, T> = MutexGuardEncodeInput<parking_lot::MutexGuard<'a, T>>;
@@ -234,13 +137,13 @@ where
 
     #[inline(always)]
     unsafe fn encoded_len_impl_raw(value: &Self::EncodeInput<'_>) -> usize {
-        let input = encode_input_from_guard::<T, _>(&value.0);
+        let input = T::encode_input_from_ref(&value.0);
         unsafe { T::encoded_len_impl_raw(&input) }
     }
 
     #[inline(always)]
     fn encode_raw_unchecked(value: Self::EncodeInput<'_>, buf: &mut impl BufMut) {
-        let input = encode_input_from_guard::<T, _>(&value.0);
+        let input = T::encode_input_from_ref(&value.0);
         T::encode_raw_unchecked(input, buf);
     }
 
@@ -252,7 +155,7 @@ where
 
     #[inline(always)]
     fn is_default_impl(value: &Self::EncodeInput<'_>) -> bool {
-        let input = encode_input_from_guard::<T, _>(&value.0);
+        let input = T::encode_input_from_ref(&value.0);
         T::is_default_impl(&input)
     }
 
@@ -264,91 +167,5 @@ where
     #[inline(always)]
     fn clear(&mut self) {
         T::clear(self.get_mut());
-    }
-}
-
-// #[cfg(feature = "parking_lot")]
-// impl<T> ProtoExt for parking_lot::Mutex<T>
-// where
-//     T: ProtoExt,
-//     for<'a> T: ProtoShadow<T, OwnedSun = T> + ProtoWire + EncodeInputFromRef<'a> + 'a,
-// {
-//     type Shadow<'a>
-//         = ParkingLotMutexShadow<<T as ProtoExt>::Shadow<'a>>
-//     where
-//         T: 'a;
-
-//     #[inline(always)]
-//     fn merge_field(
-//         value: &mut Self::Shadow<'_>,
-//         tag: u32,
-//         wire: WireType,
-//         buf: &mut impl Buf,
-//         ctx: DecodeContext,
-//     ) -> Result<(), DecodeError> {
-//         if tag == 1 {
-//             T::merge_field(&mut value.0, tag, wire, buf, ctx)
-//         } else {
-//             crate::encoding::skip_field(wire, tag, buf, ctx)
-//         }
-//     }
-// }
-
-#[cfg(feature = "parking_lot")]
-impl<SHD> ProtoWire for ParkingLotMutexShadow<SHD>
-where
-    SHD: ProtoWire,
-{
-    type EncodeInput<'b> = <SHD as ProtoWire>::EncodeInput<'b>;
-    const KIND: ProtoKind = SHD::KIND;
-
-    #[inline(always)]
-    unsafe fn encoded_len_impl_raw(value: &Self::EncodeInput<'_>) -> usize {
-        unsafe { SHD::encoded_len_impl_raw(value) }
-    }
-
-    #[inline(always)]
-    fn encode_raw_unchecked(value: Self::EncodeInput<'_>, buf: &mut impl BufMut) {
-        SHD::encode_raw_unchecked(value, buf);
-    }
-
-    #[inline(always)]
-    fn decode_into(wire_type: WireType, value: &mut Self, buf: &mut impl Buf, ctx: DecodeContext) -> Result<(), DecodeError> {
-        SHD::decode_into(wire_type, &mut value.0, buf, ctx)
-    }
-
-    #[inline(always)]
-    fn is_default_impl(value: &Self::EncodeInput<'_>) -> bool {
-        SHD::is_default_impl(value)
-    }
-
-    #[inline(always)]
-    fn proto_default() -> Self {
-        ParkingLotMutexShadow(SHD::proto_default())
-    }
-
-    #[inline(always)]
-    fn clear(&mut self) {
-        SHD::clear(&mut self.0);
-    }
-}
-
-#[cfg(feature = "parking_lot")]
-impl<SHD, T> ProtoShadow<parking_lot::Mutex<T>> for ParkingLotMutexShadow<SHD>
-where
-    SHD: ProtoShadow<T, OwnedSun = T>,
-{
-    type Sun<'a> = SHD::Sun<'a>;
-    type View<'a> = SHD::View<'a>;
-    type OwnedSun = parking_lot::Mutex<T>;
-
-    #[inline(always)]
-    fn to_sun(self) -> Result<Self::OwnedSun, DecodeError> {
-        Ok(parking_lot::Mutex::new(self.0.to_sun()?))
-    }
-
-    #[inline(always)]
-    fn from_sun(value: Self::Sun<'_>) -> Self::View<'_> {
-        SHD::from_sun(value)
     }
 }
